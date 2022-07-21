0d688f3dc766ea45a4175ec4d2fccb2bb7311336
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-07-21 20:19:32 +0100

    create button component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,10 @@
-import { Children } from "react";
-
 function Form(props) {
   return (
     <form>
       <div className="header">
         <h1>{props.header}</h1>
       </div>
-      {Children}
+      {props.children}
       <footer>Don't have an account? Sign up</footer>
     </form>
   );
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,22 @@
+import Button from "../../components/Button";
+import Form from "../../components/Form";
+
 function Login() {
-  return <h1>Login Page</h1>;
+  return (
+    <div>
+      <Form>
+        <div className="email-container">
+          <label htmlFor="email">email address</label>
+          <input type="email" name="email" />
+        </div>
+        <div className="password-container">
+          <label htmlFor="password">password</label>
+          <input type="password" name="password" />
+        </div>
+        <Button title="Log In" />
+      </Form>
+    </div>
+  );
 }
 
 export default Login;
