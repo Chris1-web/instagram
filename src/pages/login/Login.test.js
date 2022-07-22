6ae4f2d8768177546e191b0761eee5e5c8d263a4
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-07-22 17:02:00 +0100

    create login component test

--- src/pages/login/Login.test.js
+++ src/pages/login/Login.test.js
@@ -1,6 +1,7 @@
 import Login from "./Login";
 import { MemoryRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("renders login pages", () => {
   test("should have email and password input", () => {
@@ -13,5 +14,16 @@ describe("renders login pages", () => {
     const password = screen.getByLabelText(/password/i);
     expect(emailAddress).toBeInTheDocument();
     expect(password).toBeInTheDocument();
+  });
+
+  test("should have correct value of input", () => {
+    render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>
+    );
+    const emailInput = screen.getByRole("textbox", { name: "Email address" });
+    userEvent.type(emailInput, "c@gmail.com");
+    expect(emailInput.value).toBe("c@gmail.com");
   });
 });
