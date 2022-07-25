76ad965fb6295e175fc952483963cc46e20cab4f
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-07-25 21:21:34 +0100

    add firebase project and folder

---
+++ src/Firebase/Firebase-init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXZcyEZT_CD6BrhwFcjxe0ya97HR1xtOc",
+  authDomain: "instagram-cbf81.firebaseapp.com",
+  projectId: "instagram-cbf81",
+  storageBucket: "instagram-cbf81.appspot.com",
+  messagingSenderId: "142606037499",
+  appId: "1:142606037499:web:3e335c48c8e5a583371b5e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
