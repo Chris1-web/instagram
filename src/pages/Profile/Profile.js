ba7e9e7131dd05a10177310a42e615dec77627a2
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-08-09 16:53:41 +0100

    connect profile page to users collection in database

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,24 +1,104 @@
-import { auth } from "../../Firebase/Firebase-init";
-import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { SettingsOutline, BookmarkOutline, AppsOutline } from "react-ionicons";
 import user from "../../image/user.png";
+import "./Profile.css";
+import { useState } from "react";
+import Loader from "../../components/Loader/Loader";
+
+// firebase
+import { auth } from "../../Firebase/Firebase-init";
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../../Firebase/Firebase-init";
 
 function Profile() {
+  const { displayName } = useParams();
+  const [profileUser, setProfileUser] = useState(auth.currentUser);
+  const [profileInfo, setProfileInfo] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // get current user data from database
+  useState(() => {
+    const getUserProfile = async function () {
+      const userInfo = await getDoc(doc(db, "users", displayName));
+      const data = userInfo.data();
+      setProfileInfo(data);
+      setLoading(false);
+    };
+    if (loading) {
+      getUserProfile();
+    }
+    return () => getUserProfile();
+  }, [loading]);
   return (
-    <div className="top-section">
-      <>
-        <div>
-          <img
-            src={auth.currentUser.photoURL ?? user}
-            alt={auth.currentUser.displayName}
-          />
-          This contains profile image, number of followers and following, number
-          of posts, bio
+    <>
+      {loading && <Loader />}
+      {!loading && (
+        <div className="profile-section">
+          <>
+            <div className="top-section">
+              <img
+                src={profileUser.photoURL ?? user}
+                alt={profileUser.displayName}
+              />
+              <div className="right">
+                <div className="top">
+                  <h3>{profileUser.displayName}</h3>
+                  <button>Edit Profile</button>
+                  <Link to="/">
+                    <SettingsOutline
+                      color={"#00000"}
+                      height="25px"
+                      width="25px"
+                      class="icon"
+                    />
+                  </Link>
+                </div>
+                <div className="middle">
+                  <p>
+                    <span>0 </span>posts
+                  </p>
+                  <p>
+                    <span>{profileInfo.followers.length} </span>followers
+                  </p>
+                  <p>
+                    <span>{profileInfo.following.length} </span>following
+                  </p>
+                </div>
+                <div className="bottom">
+                  <p>full name</p>
+                  <p>Full stack web app developer. Bball</p>
+                </div>
+              </div>
+            </div>
+            <div className="bottom-section">
+              <ul className="tags">
+                <li className="active">
+                  <AppsOutline
+                    color={"#00000"}
+                    height="15px"
+                    width="15px"
+                    class="icon"
+                  />
+                  <span>POSTS</span>
+                </li>
+                <li>
+                  <BookmarkOutline
+                    color={"#00000"}
+                    height="15px"
+                    width="15px"
+                    class="icon"
+                  />
+                  <span>SAVED</span>
+                </li>
+              </ul>
+              <ul className="posts">
+                <p>This contains all the person's posts</p>
+              </ul>
+            </div>
+          </>
         </div>
-        <div className="bottom-section">
-          <p>This contains all the person's posts</p>
-        </div>
-      </>
-    </div>
+      )}
+    </>
   );
 }
 
