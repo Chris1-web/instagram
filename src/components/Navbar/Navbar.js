90eda76f86ab44a44a0adaf0550da343ccfed7c7
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-08-24 18:19:01 +0100

    create add new post box

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -19,13 +19,17 @@ import house from "../../image/house.jpg";
 // firebase
 import useUserStatus from "../../Hooks/useUserStatus";
 import { signOut } from "firebase/auth";
-import { auth } from "../../Firebase/Firebase-init";
+import { auth, storage } from "../../Firebase/Firebase-init";
+import { uploadBytesResumable, ref, getDownloadURL } from "firebase/storage";
 
 function Navbar() {
   const { isOnline, loading } = useUserStatus(); //custom hook
   const [currentUser, setCurrentUser] = useState(null);
   const [addNewPost, setAddNewPost] = useState(false);
-  const [newStateTwo, setNewStateTwo] = useState(true);
+  const [newStateTwo, setNewStateTwo] = useState(false);
+  const [newImage, setNewImage] = useState(null);
+  const [postingLoading, setPostingLoading] = useState(false);
+  const [disableSelector, setDisableSelector] = useState(false);
   let history = useNavigate();
 
   // if user is not online route back to login
@@ -56,7 +60,48 @@ function Navbar() {
 
   function closeAddNewPost() {
     setAddNewPost(false);
+    setNewStateTwo(false);
     hideDropdown();
+    setPostingLoading(false);
+    // if new image is not empty, delete it from storage, since it is not to be posted
+    if (newImage) {
+      setNewImage(null);
+    }
+  }
+
+  function moveToStateTwo() {
+    setAddNewPost(false);
+    setNewStateTwo(true);
+  }
+
+  function selectPostImage() {
+    const postImagePicker = document?.querySelector(".postImage-file");
+    postImagePicker.click();
+  }
+
+  async function savePostImage(event) {
+    // disable select from computer button
+    setDisableSelector(true);
+    // showing loading
+    setPostingLoading(true);
+    const file = event.target.files[0];
+    try {
+      // upload image to cloud storage
+      const filePath = `${auth.currentUser.uid}/${file.name}`;
+      const newImageRef = ref(storage, filePath);
+      await uploadBytesResumable(newImageRef, file);
+      // Generate a public URL for the file.
+      const publicImageUrl = await getDownloadURL(newImageRef);
+      // set post image in state
+      setNewImage(publicImageUrl);
+      moveToStateTwo();
+      // enable select from computer button
+      setDisableSelector(false);
+      // hide loading
+      setPostingLoading(true);
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 
   return (
@@ -158,17 +203,27 @@ function Navbar() {
                 </button>
               </div>
               <div className="add-new-post-container">
-                <h3>Create new post</h3>
+                <div className="top">
+                  <p className="empty"></p>
+                  <h3>Create new post</h3>
+                  <p className="empty"></p>
+                </div>
                 <div className="below">
                   <ImagesOutline color={"#00000"} height="80px" width="80px" />
+                  {/* loader */}
+                  {postingLoading && <span className="loader"></span>}
+                  {/* loader */}
                   {/* hidden file picker to be called when select from computer button is clicked */}
                   <input
                     type="file"
-                    id="post-file"
-                    name="post-file"
+                    className="postImage-file"
+                    name="postImage-file"
                     accept="image/png, image/jpeg"
+                    onChange={savePostImage}
                   />
-                  <button>Select From Computer</button>
+                  <button onClick={selectPostImage} disabled={disableSelector}>
+                    Select From Computer
+                  </button>
                 </div>
               </div>
             </div>
@@ -187,10 +242,14 @@ function Navbar() {
                   <button className="share">Share</button>
                 </div>
                 <div className="below-image">
-                  <img src={house} alt="house" className="postedImage" />
+                  <img src={newImage} alt="house" className="postedImage" />
                   <div className="right-side">
                     <div className="poster">
-                      <img src={user} alt="user" className="postProfileImage" />
+                      <img
+                        src={currentUser.photoURL ?? user}
+                        alt="user"
+                        className="postProfileImage"
+                      />
                       <p>{currentUser.displayName}</p>
                     </div>
                     <Form>
@@ -199,7 +258,6 @@ function Navbar() {
                         rows={26.8}
                         cols={30}
                       />
-                      {/* <input type="text" placeholder="Write a caption..." /> */}
                     </Form>
                   </div>
                 </div>
