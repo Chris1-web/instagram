8780c57184e619cb5418f869ee3ea9d82f99cfba
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-09-04 19:28:22 +0100

    edit post card design

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,6 +1,9 @@
 import { HeartOutline, ChatbubbleOutline } from "react-ionicons";
 import { Link } from "react-router-dom";
 import ProfileImage from "../ProfileImage/ProfileImage";
+import likeOutline from "../../image/like.png";
+import Love from "../../image/love.png";
+import Comment from "../../image/comment.png";
 import "./Post.css";
 
 function Post(props) {
@@ -20,8 +23,10 @@ function Post(props) {
       </header>
       <img alt="post" src={props.postPicture} className="post-image" />
       <div className="icons">
-        <HeartOutline color={"#00000"} height="30px" width="30px" />
-        <ChatbubbleOutline color={"#00000"} height="30px" width="30px" />
+        <img src={likeOutline} alt="love-outline" />
+        <img src={Comment} className="comment" alt="comment" />
+        {/* <HeartOutline color={"#00000"} height="30px" width="30px" /> */}
+        {/* <ChatbubbleOutline color={"#00000"} height="30px" width="30px" /> */}
       </div>
       <p className="picture-caption">
         <Link to={props.posterUsername} className="poster-name">
