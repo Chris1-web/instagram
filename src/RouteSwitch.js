5692727e7c7f5ee485c61de6f101b7eca32dfd2a
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-09-09 20:38:10 +0100

    changes

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -9,7 +9,7 @@ import PostDetail from "./pages/PostDetail/PostDetail";
 
 function RouteSwitch() {
   return (
-    <BrowserRouter basename="/instagram">
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Navbar />}>
           <Route index element={<Home />} />
